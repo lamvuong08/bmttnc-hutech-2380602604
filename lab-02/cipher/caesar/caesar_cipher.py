b64fa50cb80b6d35065c9be4d2ac98e58df04457
Lam Vuong <lamvuong218.hutech@gmail.com>
2026-05-26 11:58:52 +0700

    [add] lab-02 caesar,vigenere,railfence,playfair

---
+++ lab-02/cipher/caesar/caesar_cipher.py
@@ -0,0 +1,28 @@
+from cipher.caesar import ALPHABET
+
+class CaesarCipher:
+    def __init__(self):
+        self.alphabet = ALPHABET
+
+    def encrypt_text(self, text: str, key: int) -> str:
+        alphabet_length = len(self.alphabet)
+        text = text.upper()
+        encrypted_text = []
+        for char in text:
+            if char in self.alphabet:
+                letter_index = self.alphabet.index(letter)
+                output_index = (letter_index + key) % alphabet_len
+                output_letter = self.alphabet[output_index]
+                encrypted_text.append(output_letter)
+            return ''.join(encrypted_text)
+    def decrypt_text(self, text: str, key: int) -> str:
+        alphabet_length = len(self.alphabet)
+        text = text.upper()
+        decrypted_text = []
+        for char in text:
+            if char in self.alphabet:
+                letter_index = self.alphabet.index(letter)
+                output_index = (letter_index - key) % alphabet_len
+                output_letter = self.alphabet[output_index]
+                decrypted_text.append(output_letter)
+            return ''.join(decrypted_text)
